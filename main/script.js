b0ead0101de78dbac451a490b122cde5c758cd08
Asilbek <anvarsaparov374@gmail.com>
2024-02-23 14:50:53 +0500

    done

---
+++ main/script.js
@@ -0,0 +1,115 @@
+// data types - ma'lumot turlari
+// 1) primitive - string,number,boolean,null,undefined
+// 2) noprimitive
+// var
+// let
+// const
+
+// var a = 'Asilbek';
+// var b = 'Tojiyev';
+// var c = 'Shaxriyor';
+// var d = 7;
+// var e = 15;
+// var f = 25;
+// var g = true;
+// var h = false;
+// var i;
+// var j;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(j)
+// console.log(i)
+// console.log(d+e)
+// console.log(f-e)
+
+// console.log('Men' + ' ' + a + ' ' + b + ' ' + e + ' ' + 'yoshdaman.')
+
+// for (x=2; x<=20; x=+2){
+//     console.log(x)
+// }
+
+// alert("Assalomu aleykum, saytga xush kelibsiz.")
+// var ism = prompt("Isming nima?")
+// var manzil = prompt("Manziling nima?")
+// console.log(ism)
+// console.log(manzil);
+
+// var result = prompt("Yoshingiz nechada?")
+// if (result < 18){
+//     alert("Hali vaqtli ukam")
+// }
+// else if(result > 80){
+//     alert("Uje kech boboy")
+// }
+// else{
+//     alert("Xush kelibsiz, amaki")
+// }
+
+// var num = prompt("Son kiriting")
+
+// if (num > 4){
+//     alert("4 sonidan katta")
+// }
+// else if(num < 4){
+//     alert("4 sonidan kichik")
+// }
+// else{
+//     alert("4 soniga teng")
+// }
+
+var user = {
+    name: 'Asilbek',
+    lastName: 'Tojiyev',
+    age: 15,
+    isMarried: false
+}
+
+console.log(user.name)
+
+var fruits = ['Apple','Banana','Mango']
+
+console.log(fruits[0])
+// console.log(fruits[3])
+
+var student = {
+    name: 'Shaxriyor',
+    lastName: 'Baxtiyorov',
+    age: 15,
+    isMarried: false,
+    favouriteSubjects: ['English', 'Maths', 'IT']
+}
+console.log(student);
+console.log(student.favouriteSubjects[1]);
+
+var types = {
+    number:28,
+    string: 'Data',
+    boolean: true,
+    null: null,
+    undefined: undefined,
+    bigint: 9845398523987539n
+}
+console.log(types);
+
+var user = "Asilbek" // is used on old version of JavaScript
+user = "Rustam"
+console.log(user);
+
+let car = "BMW" // is used on new version of JavaScript
+car = "Audi"
+console.log(car);
+
+const id = "AA0" // unchangeable
+console.log(id);
+
+
+
+
+
---
+++ trafalgar/hello/script.js
@@ -0,0 +1,19 @@
+
+
+
+console.log('salom')
+
+
+alert('This site is really dangerous!!! If you\'re ready, OK')
+
+var ism = prompt("Type your name, bro")
+var surname = prompt("Then, type your surname")
+var age = prompt("Age????")
+console.log(ism)
+console.log(surname);
+console.log(age);
+
+
+// for (i=1;i<20;i=+2){
+//     console.log(i)
+// }
